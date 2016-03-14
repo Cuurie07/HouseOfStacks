b01ec474f2e7805cf571d1d703fc5c3526e88482
nakapika <ajay.kulkarni.1710@gmail.com>
2016-03-14 12:05:06 -0700

    UI changes

---
+++ Models/StatsData.cs
@@ -0,0 +1,19 @@
+﻿// Decompiled with JetBrains decompiler
+// Type: two.Models.StatsData
+// Assembly: two, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+// MVID: 9B0281BB-FC0F-4F23-B370-957761C517AE
+// Assembly location: E:\wwwroot\bin\two.dll
+
+using System.Collections.Generic;
+
+namespace two.Models
+{
+  public class StatsData
+  {
+    public List<LangCount> codes = new List<LangCount>();
+    public List<MapCode> mapData = new List<MapCode>();
+        public Dictionary<string, int> organization = new Dictionary<string, int>();
+        public Dictionary<string, int> people = new Dictionary<string, int>();
+        public Dictionary<string, int> location = new Dictionary<string, int>();
+    }
+}
